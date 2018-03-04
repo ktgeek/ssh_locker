a31c95f85ca5254abd7b9ddf7e096bcaecfebcef
Keith T. Garner <kgarner@kgarner.com>
2018-03-04 11:05:45 -0600

    use ssh socket protocol to remove keys
    Instead of killing the ssh-agent when a sleep or screensaver event
    happens use the ssh-agent socket and protocol to ask that the agent
    remove all the keys. This is the first pass and more cleanup will need
    to be done.

--- ssh_locker/ssh_locker.m
+++ ssh_locker/ssh_locker.m
@@ -1,19 +1,54 @@
 // Original implementation by Dave Dribin in 2007. Modifications to
 // add locking on screensaver by Lena Singer and Keith Garner in 2011
+// Further changes by Keith Garner in 2018
 //
 #import <Foundation/Foundation.h>
 
 #import "myListener.h"
+#include "sys/types.h"
+#include "sys/un.h"
+#include "sys/socket.h"
 
-int stop_agent(void)
+#define SSH_AGENT_SUCCESS 0x6
+#define SSH_AGENTC_REMOVE_ALL_IDENTITIES 0x13
+
+void remove_keys_from_agent(void)
 {
-    NSArray * arguments = [NSArray arrayWithObjects: @"stop",
-                           @"com.openssh.ssh-agent", nil];
-    
-    NSTask * stopAgent = [NSTask launchedTaskWithLaunchPath: @"/bin/launchctl"
-                                                  arguments: arguments];
-    [stopAgent waitUntilExit];
-    return [stopAgent terminationStatus];
+    NSString *socketPath = [[NSProcessInfo processInfo] environment][@"SSH_AUTH_SOCK"];
+
+    struct sockaddr_un socketInfo;
+    socketInfo.sun_family = AF_UNIX;
+    [socketPath getCString:socketInfo.sun_path maxLength:104 encoding:NSASCIIStringEncoding];
+    socketInfo.sun_len = SUN_LEN(&socketInfo);
+
+    NSLog(@"About to lock using %s", socketInfo.sun_path);
+
+    int ssh_agent_socket = socket(PF_UNIX, SOCK_STREAM, 0);
+    connect(ssh_agent_socket, (struct sockaddr *)&socketInfo, socketInfo.sun_len);
+
+    // Per ssh communication, uint32 of message length (in our case the one byte following) and
+    // then the message type which is SSH_AGENTC_REMOVE_ALL_IDENTITIES
+    int8_t buffer[5];
+    uint32_t messageLength = htonl(1);
+    memcpy(buffer, &messageLength, 4);
+    buffer[4] = SSH_AGENTC_REMOVE_ALL_IDENTITIES;
+
+    send(ssh_agent_socket, buffer, 5, 0);
+    recv(ssh_agent_socket, buffer, 5, MSG_WAITALL);
+
+    memcpy(&messageLength, buffer, 4);
+    messageLength = ntohl(messageLength);
+
+    if ((messageLength != 1) || (buffer[4] != SSH_AGENT_SUCCESS))
+    {
+        NSLog(@"Failure removing keys from agent");
+    }
+    else
+    {
+        NSLog(@"Successfully removed keys");
+    }
+
+    close(ssh_agent_socket);
 }
 
 
@@ -44,11 +79,7 @@ int stop_agent(void)
 
 - (void)screensaverOrScreenLocked:(NSNotification *)theNotification
 {
-#if 1
-    stop_agent();
-#else
-    NSLog(@"stop_agent: %d",  stop_agent());
-#endif
+    remove_keys_from_agent();
 }
 
 @end
@@ -56,7 +87,7 @@ int stop_agent(void)
 int main (int argc, const char * argv[])
 {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    
+
     [[myListener alloc] init];
 
     [[NSRunLoop currentRunLoop] run];
